86db7b5ca314d189d0e7b5eb28af9979fb30fbc5
JunJangE <wnswkd486@gmail.com>
2023-03-02 20:37:01 +0900

    Update 가장 큰 정사각형 찾기.py

--- 조준장/level2/가장 큰 정사각형 찾기.py
+++ 조준장/level2/가장 큰 정사각형 찾기.py
@@ -1,8 +1,8 @@
 def solution(board):
     answer = 0
     
-    for i in range(len(board)):
-        for j in range(len(board[0])):
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
             if board[i][j] == 1:
                 board[i][j] = min(board[i-1][j], board[i][j-1], board[i-1][j-1]) + 1
     
