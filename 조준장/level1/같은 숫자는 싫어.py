378e0eeded0eeecffc95ee68301c28820c26a24e
JunJangE <wnswkd486@gmail.com>
2023-03-09 08:53:07 +0900

    Create 같은 숫자는 싫어.py

---
+++ 조준장/level1/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for a in arr:
+        if not answer:
+            answer.append(a)
+            continue
+            
+        if answer[-1] != a:
+            answer.append(a)
+            
+    return answer
