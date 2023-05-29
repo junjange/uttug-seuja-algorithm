a7c0bf99f1f20d1c1b83e4e1793bc5131a5ea47d
Lee hunil <104710245+Leehunil@users.noreply.github.com>
2023-05-29 19:00:00 +0900

    Create 가장 큰 수 찾기.java

---
+++ 이훈일/Programmers/Level0/가장 큰 수 찾기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] array) {
+        List<Integer> intList = Arrays.stream(array).boxed().collect(Collectors.toList());
+        int max = intList.stream().mapToInt(x -> x).max().getAsInt();
+        int sex = intList.indexOf(max);
+        return new int[]{max, sex};
+    }
+}
