9bf87fbb29e4ec69ab16b54756ad8d7940cf03cf
chanu2 <96942183+chanu2@users.noreply.github.com>
2023-06-23 23:25:30 +0900

    Create 없는숫자더하기.java

---
+++ 김찬우/프로그래머스 /Lv.1/없는숫자더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int result = 45;
+        
+        for(int i=0;i<numbers.length;i++){
+            
+            result-=numbers[i];
+            
+        }
+        
+        return result;
+    }
+}
