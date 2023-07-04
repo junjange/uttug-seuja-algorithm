747fc12fe156b5f38889107dab05273a6571981a
Lee hunil <104710245+Leehunil@users.noreply.github.com>
2023-07-04 16:36:00 +0900

    Create 최소직사각형.java

---
+++ 이훈일/Programmers/Level1/최소직사각형.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int[] max = new int[sizes.length];
+        int[] min = new int[sizes.length];
+        
+        for(int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] > sizes[i][1]){
+                max[i] = sizes[i][0];
+                min[i] = sizes[i][1];
+            } else{
+                max[i] = sizes[i][1];
+                min[i] = sizes[i][0];
+            }
+        }
+        
+        Arrays.sort(max);
+        Arrays.sort(min);
+        
+        return max[max.length - 1] * min[min.length - 1];
+    }
+}
