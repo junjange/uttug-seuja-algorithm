3738e687a78bd475b32efd2e783000c2e4021121
Lee hunil <104710245+Leehunil@users.noreply.github.com>
2023-11-09 23:50:34 +0900

    Create 5549.java

---
+++ 이훈일/삼성 SW 역량 테스트 기출 문제/5549.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			String number = sc.next();
+            int word = (int) number.charAt(number.length()-1);
+            if(word % 2 == 0) {
+            	System.out.println("#" + test_case + " " + "Even");
+            } else {
+            	System.out.println("#" + test_case + " " + "Odd");
+            }
+            
+		}
+	}
+}
