52c81645b48a627194b74921909a7f2717ff0cda
chanu2 <96942183+chanu2@users.noreply.github.com>
2023-11-20 00:03:28 +0900

    Update 베스트앨범.java

--- 김찬우/프로그래머스 /Lv.3/베스트앨범.java
+++ 김찬우/프로그래머스 /Lv.3/베스트앨범.java
@@ -1,25 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
+    public List<Integer> solution(String[] genres, int[] plays) {
 
         
         Map<String, HashMap<Integer,Integer>> map = new HashMap<>();
         
         HashMap<String,Integer> hash = new HashMap<>();
         
+        List<Integer> result = new ArrayList<>();
+        
         
         for(int i =0; i < genres.length; i++){
             
-
             if(hash.containsKey(genres[i])){
                 hash.put(genres[i], hash.get(genres[i]) + plays[i]);
-                hashMap.put(i,plays[i]);
+                map.get(genres[i]).put(i,plays[i]);
+                
             }else{
                 HashMap<Integer,Integer> hashMap = new HashMap<>();
                 hashMap.put(i,plays[i]);
                 hash.put(genres[i], plays[i]);
-                //map.put(genres[i],)
+                map.put(genres[i],hashMap);
             }       
         }
         
@@ -27,35 +29,31 @@ class Solution {
         
         list.sort((o1,o2) -> hash.get(o2) - hash.get(o1));
         
-        for(int i=0; i<list.size(); i++){
+   
+        System.out.println(map);
+  
+        
+        for(int i = 0; i<list.size(); i++){
             
-            HashMap<Integer,Integer> hashMap = new HashMap<>();
             String key = list.get(i);
             
-            for(int j=0; j<genres.length; j++){
-                
-                if(key.equals(genres[j])){
-                    hashMap.put(j,plays[j]);
-                    
-                }else{
-                    continue;
-                }
-                
-            }
-            map.put(key,hashMap);
+            HashMap<Integer,Integer> newMap = map.get(key);
             
+            System.out.println(newMap);
+        
+            List<Integer> li = new ArrayList<>(newMap.keySet());
+            
+            li.sort((o1,o2) -> newMap.get(o2) - newMap.get(o1));
+            
+            result.add(li.get(0));
+            
+            if(li.size() > 1){
+                result.add(li.get(1));
+            }
             
         }
-        
-         List<String> keys =  new ArrayList<>(map.keySet());
-        
-      
-        
-        
-        
 
-        int[] answer = {};
         
-        return answer;
+        return result;
     }
 }
