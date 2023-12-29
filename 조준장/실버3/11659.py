e7579476255970384501720b574e148dfb540843
JunJangE <wnswkd486@gmail.com>
2023-12-29 21:32:48 +0900

    Create 11659.py

---
+++ 조준장/실버3/11659.py
@@ -0,0 +1,14 @@
+"""
+- 누적합
+"""
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+nums = [0] + list(map(int, sys.stdin.readline().split()))
+
+for i in range(1, n + 1):
+    nums[i] += nums[i - 1]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    print(nums[b] - nums[a - 1])
