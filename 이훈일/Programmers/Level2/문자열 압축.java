806286b78c957d532c74ad172dabe5f800e204af
Lee hunil <104710245+Leehunil@users.noreply.github.com>
2024-09-19 15:30:01 +0900

    Update 문자열 압축.java

--- 이훈일/Programmers/Level2/문자열 압축.java
+++ 이훈일/Programmers/Level2/문자열 압축.java
@@ -10,9 +10,8 @@ class Solution {
     
     public static int compress(String s, int len) {
         String pre = s.substring(0, len);
-        boolean isSame = false;
         int totalCnt = 0;
-        int sameCnt = 0;
+        int sameCnt = 1;
         for(int i = len; i < s.length(); i += len) {
             String cur = "";
             if(i+len > s.length()) {
@@ -22,29 +21,20 @@ class Solution {
             }
             
             if(!pre.equals(cur)) {
-                if(isSame) {
+                if(sameCnt > 1) {
                     totalCnt += len + String.valueOf(sameCnt).length();
                 } else {
                     totalCnt += len;
                 }
-                isSame = false;
+                sameCnt = 1;
             } else {
-                if(isSame) {
-                    sameCnt++;
-                } else {
-                    isSame = true;
-                    sameCnt = 2;
-                }
+                sameCnt++;
             }
             pre = cur;
         }
         
-        if(isSame) {
-            if(sameCnt > 9) {
-                totalCnt += len + String.valueOf(sameCnt).length();
-            } else {
-                totalCnt += len + 1;
-            }
+        if(sameCnt > 1) {
+            totalCnt += pre.length() + String.valueOf(sameCnt).length();
         } else {
             totalCnt += pre.length();
         }
